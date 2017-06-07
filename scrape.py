5c274ec69a2c4354c71628e2b22ebc27a064b231
bobfishtincar <bhavishdinakar@berkeley.edu>
2017-06-06 22:14:50 -0700

    works with basic functionality

--- scrape.py
+++ scrape.py
@@ -1,12 +1,23 @@
 from lxml import html
 import requests
 
-page = requests.get('https://twitter.com/search?q=i%20wish%20there%20was%20app&src=typd')
+scrape_search = input('Phrase to search: ')
+scrape_string = scrape_search.replace(' ', '%20')
+
+page = requests.get('https://twitter.com/search?q=' + scrape_string)
+
 html_text = str(page.content)
 html_text = html_text.replace('<strong>', '')
 html_text = html_text.replace('</strong>', '')
+
 tree = html.fromstring(html_text)
 
 tweets = tree.xpath('//div[@class="js-tweet-text-container"]/p/text()')
 
-print(tweets)
+with open('savedtweets.txt', 'w') as my_file:
+    for tweet in tweets:
+        print(tweet)
+        want_save = input('Save?')
+
+        if len(want_save) > 0:
+            my_file.write(tweet + '\n')
